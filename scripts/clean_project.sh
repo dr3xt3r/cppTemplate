c8834150b256b6c4ff1103cc8f9df2d1f80aac9a
dr3xt3r <richard.vano8@gmail.com>
2024-10-18 19:47:18 +0000

    clean project script and task

---
+++ scripts/clean_project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to clear a folder but keep the folder itself
+clear_folder() {
+# specify local variable 'folder' as the first argument passed to the function
+  local folder=$1
+  if [ -d "$folder" ]; then
+    echo "Clearing contents of $folder..."
+# remove all files, hidden files and directories in the specified folder
+    rm -rf "$folder"/*
+    rm -rf "$folder"/.[!.]*
+    echo "$folder cleared!"
+  else
+    echo "$folder does not exist"
+  fi
+}
+
+
+# Clear build directory
+clear_folder "build"
+
+# Clear bin directory
+clear_folder "bin"
+
+# Clear .cache directory
+clear_folder ".cache"
+
+# Optionally, clear any additional folders here
+echo "All folders cleaned!"

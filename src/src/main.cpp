3036826936c030f8c13f02f56289a92a731f1fff
dr3xt3r <richard.vano8@gmail.com>
2024-10-20 16:02:43 +0000

    reorganized structure of src files

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 
 #include "calculator.h"
@@ -7,6 +8,8 @@ int main()
 	Calculator calc;
 	std::cout << "3 + 4 = " << calc.add(7, 8) << std::endl;
 	std::cout << "10 - 2 = " << calc.subtract(10, 2) << std::endl;
+
+    int* var = (int*)malloc(sizeof(int));
 
 	return 0;
 }

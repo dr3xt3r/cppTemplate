453162d41fdec7c891158b8e035532fea03ce9e8
dr3xt3r <richard.vano8@gmail.com>
2024-11-18 22:45:44 +0000

    update compiler flags and tests

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -15,6 +15,18 @@ TEST(HelloTest, AddNegativeNumbers) {
     EXPECT_EQ(calc.add(-5, -7), -12);
 }
 
+TEST(HelloTest, SubtractPositiveNumbers) {
+    Calculator calc;
+    EXPECT_EQ(calc.subtract(1, 2), -1);
+    EXPECT_EQ(calc.subtract(5, 7), -2);
+}
+
+TEST(HelloTest, SubtractNegativeNumbers) {
+    Calculator calc;
+    EXPECT_EQ(calc.subtract(-1, -2), 1);
+    EXPECT_EQ(calc.subtract(-5, -7), 2);
+}
+
 // Main function for GoogleTest
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);

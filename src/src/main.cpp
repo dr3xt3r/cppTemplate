06de8f67086e50b830af0a3c8949bac2a4a02794
dr3xt3r <richard.vano8@gmail.com>
2024-11-24 21:12:23 +0000

    some changes to example code

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -9,7 +9,5 @@ int main()
 	std::cout << "3 + 4 = " << calc.add(7, 8) << std::endl;
 	std::cout << "10 - 2 = " << calc.subtract(10, 2) << std::endl;
 
-    int* var = (int*)malloc(sizeof(int));
-
 	return 0;
 }
